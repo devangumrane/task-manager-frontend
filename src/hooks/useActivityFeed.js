d452008b37d2419a6c6608d6315d52ee3c7559e4
Devang Umrane <devangumrane8888@gmail.com>
2025-12-22 17:38:16 +0530

    activity-log-process

--- src/hooks/useActivityFeed.js
+++ src/hooks/useActivityFeed.js
@@ -1,10 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
 import { getActivity } from "../services/activityService";
+import { useAuthStore } from "../store/authStore";
 
 export const useActivityFeed = (workspaceId) => {
+  const { accessToken, isBootstrapped } = useAuthStore();
+
   return useQuery({
     queryKey: ["workspaceActivity", workspaceId],
     queryFn: () => getActivity(workspaceId),
-    enabled: !!workspaceId,
+    enabled: !!workspaceId && !!accessToken && isBootstrapped,
   });
 };
--- src/pages/ActivityPage.jsx
+++ src/pages/ActivityPage.jsx
@@ -1,3 +1,7 @@
+console.log("ACTIVITY PAGE PARAMS", {
+  workspaceId,
+});
+
 export default function ActivityPage() {
   return <div>Activity Log</div>;
 }
